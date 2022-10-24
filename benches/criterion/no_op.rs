4e0e83777501553f9426ec791dc6a4bb61ce3f62
ollie-etl <Oliver Bunting@etlsystems.com>
2022-10-24 20:31:01 +0100

    Use async benchmarker

--- benches/criterion/no_op.rs
+++ benches/criterion/no_op.rs
@@ -1,16 +1,20 @@
 use criterion::{
-    criterion_group, criterion_main, BenchmarkId, Criterion, SamplingMode, Throughput,
+    criterion_group, criterion_main, BenchmarkId, Criterion, Throughput,
 };
-use std::time::{Duration, Instant};
-
 use futures::stream::{self, StreamExt};
+
+struct AsyncRuntime(tokio_uring::Runtime);
+
+impl criterion::async_executor::AsyncExecutor for &AsyncRuntime {
+    fn block_on<T>(&self, future: impl futures::Future<Output = T>) -> T {
+        self.0.block_on(future)
+    }
+}
 
 #[derive(Clone)]
 struct Options {
     iterations: usize,
     concurrency: usize,
-    sq_size: usize,
-    cq_size: usize,
 }
 
 impl Default for Options {
@@ -18,49 +22,39 @@ impl Default for Options {
         Self {
             iterations: 100000,
             concurrency: 1,
-            sq_size: 128,
-            cq_size: 256,
         }
     }
 }
 
-fn run_no_ops(opts: &Options, count: u64) -> Duration {
-    let mut ring_opts = tokio_uring::uring_builder();
-    ring_opts
-        .setup_cqsize(opts.cq_size as _)
-        // .setup_sqpoll(10)
-        // .setup_sqpoll_cpu(1)
-        ;
-
-    let mut m = Duration::ZERO;
-
-    // Run the required number of iterations
-    for _ in 0..count {
-        m += tokio_uring::builder()
-            .entries(opts.sq_size as _)
-            .uring_builder(&ring_opts)
-            .start(async move {
-                let start = Instant::now();
-                stream::iter(0..opts.iterations)
-                    .for_each_concurrent(Some(opts.concurrency), |_| async move {
-                        tokio_uring::no_op().await.unwrap();
-                    })
-                    .await;
-                start.elapsed()
-            })
-    }
-    m
+async fn run_no_ops(opts: &Options) {
+    stream::iter(0..opts.iterations)
+        .for_each_concurrent(Some(opts.concurrency), |_| async move {
+            tokio_uring::no_op().await.unwrap();
+        })
+        .await
 }
 
 fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("no_op");
     let mut opts = Options::default();
+
+    let mut ring_opts = tokio_uring::uring_builder();
+    ring_opts
+        .setup_cqsize(256)
+        // .setup_sqpoll(10)
+        // .setup_sqpoll_cpu(1)
+        ;
+
+    let mut builder = tokio_uring::builder();
+    builder.entries(128).uring_builder(&ring_opts);
+
+    let runtime = AsyncRuntime(tokio_uring::Runtime::new(&builder).unwrap());
+    (runtime);
+
     for concurrency in [1, 32, 64, 256].iter() {
         opts.concurrency = *concurrency;
 
         // We perform long running benchmarks: this is the best mode
-        group.sampling_mode(SamplingMode::Flat);
-
         group.throughput(Throughput::Elements(opts.iterations as u64));
         group.bench_with_input(
             BenchmarkId::from_parameter(concurrency),
@@ -68,12 +62,16 @@ fn bench(c: &mut Criterion) {
             |b, opts| {
                 // Custom iterator used because we don't expose access to runtime,
                 // which is required to do async benchmarking with criterion
-                b.iter_custom(move |iter| run_no_ops(opts, iter));
+                b.to_async(&runtime).iter(|| run_no_ops(opts));
             },
         );
     }
     group.finish();
 }
 
-criterion_group!(benches, bench);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = bench
+}
 criterion_main!(benches);
