c13ecf1809f1cff10170350e23330346edcb18b8
MA-ETL <Mark.Adair@etlsystems.com>
2023-01-19 09:31:20 +0000

    Update

--- src/io/sendmsg_zc.rs
+++ src/io/sendmsg_zc.rs
@@ -87,10 +87,12 @@ impl<T, U> Completable for SendMsgZc<T, U> {
 
     fn complete(self, cqe: CqeResult) -> (io::Result<usize>, Vec<T>, Option<U>) {
         // Convert the operation result to `usize`
-        let res = cqe.result.map(|v| v as usize);
+        let mut res = cqe.result.map(|v| v as usize);
 
         // Add the number of bytes to res.
-        res += self.bytes as usize;
+        match res {
+            Ok(_res) => res = Ok(_res + self.bytes),
+        }
 
         // Recover the data buffers.
         let io_bufs = self.io_bufs;
