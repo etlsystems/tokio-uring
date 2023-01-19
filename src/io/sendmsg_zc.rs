c5edf1a67737cf9cd8ae01aaebb3a3a6bbfdd90d
MA-ETL <Mark.Adair@etlsystems.com>
2023-01-19 14:12:18 +0000

    Update

--- src/io/sendmsg_zc.rs
+++ src/io/sendmsg_zc.rs
@@ -42,6 +42,11 @@ impl<T: IoBuf, U: IoBuf> Op<SendMsgZc<T, U>, MultiCQEFuture> {
             }))
         }
 
+        println!("io_slices.as_ptr() = {:?}", io_slices.as_ptr());
+
+        println!("io_slices[0].as_ptr() = {:?}, io_slices[0].len() = {}", io_slices[0].as_ptr(), io_slices[0].len());
+        println!("io_slices[1].as_ptr() = {:?}, io_slices[1].len() = {}", io_slices[0].as_ptr(), io_slices[0].len());
+
         msghdr.msg_iov = io_slices.as_ptr() as *mut _;
         msghdr.msg_iovlen = io_slices.len() as _;
         msghdr.msg_name = socket_addr.as_ptr() as *mut libc::c_void;
