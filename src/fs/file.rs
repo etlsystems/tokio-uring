600bf9758ea12314975ce55143a319cdabd417a8
ollie-etl <72926894+ollie-etl@users.noreply.github.com>
2023-08-16 11:47:54 +0000

    Mark api as unsafe, and provide a metadata store

--- src/fs/file.rs
+++ src/fs/file.rs
@@ -183,8 +183,16 @@ impl File {
     }
 
     /// A file/device-specific 16-byte command, akin (but not equivalent) to ioctl(2).
-    pub async fn uring_cmd16(&self, cmd_op: u32, cmd: [u8; 16]) -> io::Result<u32> {
-        let op = Op::uring_cmd16(&self.fd, cmd_op, cmd).unwrap();
+    ///
+    /// Marked as unsafe, as it is the callers responsibility to ensure that any data which is required
+    /// to be stable is either passed in as the metadata argument, or otherwise stable through the life of the operation
+    pub async unsafe fn uring_cmd16<T: std::marker::Unpin>(
+        &self,
+        cmd_op: u32,
+        cmd: [u8; 16],
+        metadata: T,
+    ) -> (io::Result<u32>, T) {
+        let op = Op::uring_cmd16(&self.fd, cmd_op, cmd, metadata).unwrap();
         op.await
     }
 
--- src/io/uring_cmd.rs
+++ src/io/uring_cmd.rs
@@ -3,25 +3,31 @@ use crate::runtime::driver::op::{Completable, CqeResult, Op};
 use crate::runtime::CONTEXT;
 use std::io;
 
-pub(crate) struct UringCmd16 {
+pub(crate) struct UringCmd16<T> {
     /// Holds a strong ref to the FD, preventing the file from being closed
     /// while the operation is in-flight.
     #[allow(dead_code)]
     fd: SharedFd,
+    // Holds any required metadata stable for the lifetime of the operation
+    metadata: T,
 }
 
-impl Op<UringCmd16> {
+impl<T> Op<UringCmd16<T>> {
     /// A file/device-specific 16-byte command, akin (but not equivalent) to ioctl
     pub(crate) fn uring_cmd16(
         fd: &SharedFd,
         cmd_op: u32,
         cmd: [u8; 16],
-    ) -> io::Result<Op<UringCmd16>> {
+        metadata: T,
+    ) -> io::Result<Op<UringCmd16<T>>> {
         use io_uring::{opcode, types};
 
         CONTEXT.with(|x| {
             x.handle().expect("Not in a runtime context").submit_op(
-                UringCmd16 { fd: fd.clone() },
+                UringCmd16 {
+                    fd: fd.clone(),
+                    metadata,
+                },
                 |_| {
                     opcode::UringCmd16::new(types::Fd(fd.raw_fd()), cmd_op)
                         .cmd(cmd)
@@ -32,11 +38,11 @@ impl Op<UringCmd16> {
     }
 }
 
-impl Completable for UringCmd16 {
-    type Output = io::Result<u32>;
+impl<T> Completable for UringCmd16<T> {
+    type Output = (std::result::Result<u32, io::Error>, T);
 
     fn complete(self, cqe: CqeResult) -> Self::Output {
-        cqe.result
+        (cqe.result, self.metadata)
     }
 }
 
