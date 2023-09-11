749babcbf6166d3926e56539f22fa0261b6d7ef6
MA-ETL <Mark.Adair@etlsystems.com>
2023-09-11 15:04:40 +0100

    Update

--- src/net/xdp.rs
+++ src/net/xdp.rs
@@ -9,6 +9,12 @@ use std::os::raw::c_char;
 
 //use libxdp_sys::{_xsk_ring_cons__peek, _xsk_ring_cons__release, _xsk_ring_cons__rx_desc};
 
+const XSK_RING_CONS__DEFAULT_NUM_DESCS: u32 = 2048;
+const XSK_RING_PROD__DEFAULT_NUM_DESCS: u32 = 2048;
+
+const XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD: u32 = (1 << 0);
+const XSK_LIBXDP_FLAGS__INHIBIT_PROG_LOAD: u32 = (1 << 0);
+
 pub struct xsk_socket {}
 
 struct xsk_umem {}
@@ -17,7 +23,18 @@ pub struct xsk_ring_prod {}
 
 pub struct xsk_ring_cons {}
 
-pub struct xsk_socket_config {}
+union xsk_socket_config_union {
+    libbpf_flags: u32,
+    libxdp_flags: u32,
+}
+
+pub struct xsk_socket_config {
+    pub rx_size: u32,
+    pub tx_size: u32,
+    pub lib_flags: xsk_socket_config_union,
+    pub xdp_flags: u32,
+    pub bind_flags: u16,
+}
 
 pub struct XdpSocket {
     pub(super) inner: Socket,
@@ -35,12 +52,35 @@ impl XdpSocket {
         fill: *mut xsk_ring_prod,
         comp: *mut xsk_ring_cons,
         usr_config: *const xsk_socket_config,
-    ) {
+    ) -> i32 {
+        // Check that we have the necessary valid pointers.
+        if !umem || !xsk_ptr || !(rx || tx) {
+            return libc::EFAULT;
+        }
+
         // Calloc size of xsk socket struct
+        let mut xsk = Box::new();
 
         // Set xdp_socket_config
 
         // Check if umem refcount
+    }
+
+    pub fn set_socket_config(
+        cfg: &mut xsk_socket_config,
+        usr_cfg: *const cxsk_socket_config,
+    ) -> i32 {
+        if (!usr_cfg) {
+            cfg.rx_size = XSK_RING_CONS__DEFAULT_NUM_DESCS;
+            cfg.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS;
+            cfg.libbpf_flags = 0;
+            cfg.xdp_flags = 0;
+            cfg.bind_flags = 0;
+        }
+
+        if (usr_cfg.libbpf_flags & !XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD) {
+            return libc::EINVAL;
+        }
     }
 
     pub async fn sendmsg() {
