ad028671fd978b0f5be77e3762d308e1a22d5c5a
MA-ETL <Mark.Adair@etlsystems.com>
2023-09-12 15:47:39 +0100

    Update

--- src/net/xdp.rs
+++ src/net/xdp.rs
@@ -1,4 +1,4 @@
-use libc::SOL_XDP;
+use libc::{sockaddr, MAP_FAILED, MAP_POPULATE, MAP_SHARED, PROT_READ, PROT_WRITE, SOL_XDP};
 
 use crate::{
     buf::fixed::FixedBuf,
@@ -7,7 +7,7 @@ use crate::{
     UnsubmittedWrite,
 };
 
-use std::os::raw::c_char;
+use std::{f64::consts, os::raw::c_char};
 
 //use libxdp_sys::{_xsk_ring_cons__peek, _xsk_ring_cons__release, _xsk_ring_cons__rx_desc};
 
@@ -39,6 +39,13 @@ const XDP_UMEM_FILL_RING: u32 = 5;
 const XDP_UMEM_COMPLETION_RING: u32 = 6;
 const XDP_STATISTICS: u32 = 7;
 const XDP_OPTIONS: u32 = 8;
+
+const XDP_PGOFF_RX_RING: u32 = 0;
+const XDP_PGOFF_TX_RING: u32 = 0x80000000;
+const XDP_UMEM_PGOFF_FILL_RING: u32 = 0x100000000;
+const XDP_UMEM_PGOFF_COMPLETION_RING: u32 = 0x180000000;
+
+const XDP_SHARED_UMEM: u32 = (1 << 0);
 
 pub struct xsk_ctx {
     fill: *mut xsk_ring_prod,
@@ -127,10 +134,24 @@ pub struct xdp_ring_offset {
 }
 
 pub struct xdp_mmap_offsets {
-    rx: xdp_ring_offset,
-    tx: xdp_ring_offset,
-    fr: xdp_ring_offset,
-    cr: xdp_ring_offset,
+    pub rx: xdp_ring_offset,
+    pub tx: xdp_ring_offset,
+    pub fr: xdp_ring_offset,
+    pub cr: xdp_ring_offset,
+}
+
+struct xdp_desc {
+    pub addr: u64,
+    pub len: u32,
+    pub options: u32,
+}
+
+pub struct sockaddr_xdp {
+    pub sxdp_family: u16,
+    pub sxdp_flags: u16,
+    pub sxdp_ifindex: u32,
+    pub sxdp_queue_id: u32,
+    pub sxdp_shared_umem_fd: u32,
 }
 
 pub struct XdpUmem {}
@@ -209,6 +230,10 @@ impl XdpSocket {
         let mut ifindex: i32 = 0;
         let mut netns_cookie: u64 = 0;
         let mut optlen: u32 = 0;
+        let mut off: xdp_mmap_offsets;
+        let mut rx_map: *mut std::ffi::c_void = std::ptr::null_mut();
+        let mut tx_map: *mut std::ffi::c_void = std::ptr::null_mut();
+        let mut sxdp: sockaddr_xdp;
 
         // Check that we have the necessary valid pointers.
         if !umem || xsk_ptr.is_null() || (rx.is_null() && tx.is_null()) {
@@ -216,7 +241,7 @@ impl XdpSocket {
         }
 
         // Calloc size of xsk socket struct
-        let mut xsk: Box<xsk_socket> = Default::default();
+        let mut xsk: Box<xsk_socket>; // = Default::default();
 
         // Set xdp_socket_config
         err = XdpSocket::set_socket_config(&mut xsk.config, usr_config);
@@ -315,12 +340,116 @@ impl XdpSocket {
         }
 
         // Get mmap offsets
+        err = xsk_get_mmap_offsets(xsk.fd, &mut off);
+
+        if err != 0 {
+            err = 0 - std::io::Error::last_os_error().raw_os_error().unwrap();
+            // goto out_put_ctx;
+        }
 
         // rx mmap
-        if !rx.is_null() {}
+        if !rx.is_null() {
+            unsafe {
+                rx_map = libc::mmap(
+                    std::ptr::null_mut() as *mut std::ffi::c_void,
+                    (off.rx.desc as usize)
+                        + (xsk.config.rx_size as usize) * std::mem::size_of::<xdp_desc>(),
+                    PROT_READ | PROT_WRITE,
+                    MAP_SHARED | MAP_POPULATE,
+                    xsk.fd,
+                    XDP_PGOFF_RX_RING as i64,
+                );
+            }
+
+            if rx_map == MAP_FAILED {
+                err = 0 - std::io::Error::last_os_error().raw_os_error().unwrap();
+                // goto out_put_ctx;
+            }
+
+            unsafe {
+                (*rx).mask = xsk.config.rx_size - 1;
+                (*rx).size = xsk.config.rx_size;
+                (*rx).producer = rx_map.wrapping_add(off.rx.producer as usize) as *mut u32;
+                (*rx).consumer = rx_map.wrapping_add(off.rx.consumer as usize) as *mut u32;
+                (*rx).flags = rx_map.wrapping_add(off.rx.flags as usize) as *mut u32;
+                (*rx).ring = rx_map.wrapping_add(off.rx.desc as usize);
+                (*rx).cached_prod = *(*rx).producer;
+                (*rx).cached_cons = *(*rx).consumer;
+            }
+        }
+
+        xsk.rx = rx;
 
         // tx mmap
-        if !tx.is_null() {}
+        if !tx.is_null() {
+            unsafe {
+                tx_map = libc::mmap(
+                    std::ptr::null_mut() as *mut std::ffi::c_void,
+                    (off.tx.desc as usize)
+                        + (xsk.config.tx_size as usize) * std::mem::size_of::<xdp_desc>(),
+                    PROT_READ | PROT_WRITE,
+                    MAP_SHARED | MAP_POPULATE,
+                    xsk.fd,
+                    XDP_PGOFF_TX_RING as i64,
+                );
+            }
+
+            if tx_map == MAP_FAILED {
+                err = 0 - std::io::Error::last_os_error().raw_os_error().unwrap();
+                // goto out_mmap_rx;
+            }
+
+            unsafe {
+                (*tx).mask = xsk.config.tx_size - 1;
+                (*tx).size = xsk.config.tx_size;
+                (*tx).producer = tx_map.wrapping_add(off.tx.producer as usize) as *mut u32;
+                (*tx).consumer = tx_map.wrapping_add(off.tx.consumer as usize) as *mut u32;
+                (*tx).flags = tx_map.wrapping_add(off.tx.flags as usize) as *mut u32;
+                (*tx).ring = tx_map.wrapping_add(off.tx.desc as usize);
+                (*tx).cached_prod = *(*tx).producer;
+                (*tx).cached_cons = *(*tx).consumer + xsk.config.tx_size;
+            }
+        }
+
+        xsk.tx = tx;
+
+        // Setup sockaddr
+        sxdp.sxdp_family = PF_XDP as u16;
+        //sxdp.sxdp_ifindex =
+        //sxdp.sxdp_queue_id =
+
+        if (umem.refcount > 1) {
+            sxdp.sxdp_flags |= XDP_SHARED_UMEM;
+            sxdp.sxdp_shared_umem_fd = umem.fd as u32;
+        } else {
+            sxdp.sxdp_flags = xsk.config.bind_flags;
+        }
+
+        // Bind socket
+        unsafe {
+            err = libc::bind(
+                xsk.fd,
+                &sxdp as *const sockaddr_xdp as *const std::ffi::c_void as *const sockaddr,
+                std::mem::size_of::<sockaddr_xdp>() as u32,
+            );
+        }
+
+        if err != 0 {
+            err = 0 - std::io::Error::last_os_error().raw_os_error().unwrap();
+            // goto out_mmap_tx;
+        }
+
+        // Setup xdp prog
+        if (xsk.config.lib_flags.libbpf_flags & XSK_LIBBPF_FLAGS__INHIBIT_PROG_LOAD) != 0 {
+            // err = xsk_setup_xdp_prog(xsk);
+        }
+
+        unsafe {
+            (*xsk_ptr) = xsk.as_mut();
+        }
+
+        umem.fill_save = std::ptr::null_mut();
+        umem.comp_save = std::ptr::null_mut();
 
         return 0;
     }
